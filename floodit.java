0f24ea0586d54405e14bbca0f2710de9cad71260
Raza (Htet) Hlaing <107883143+razahtet@users.noreply.github.com>
2025-06-12 23:12:30 -0400

    Update with java swing

--- floodit.java
+++ floodit.java
@@ -1,7 +1,9 @@
-import tester.Tester;
-import javalib.impworld.*;
-import java.awt.Color;
-import javalib.worldimages.*;
+package src;
+
+import javax.swing.*;
+import javax.swing.Timer;
+import java.awt.*;
+import java.awt.event.*;
 import java.util.*;
 
 // Represents a single square of the game area
@@ -18,7 +20,7 @@ class Cell {
   Cell bottom;
 }
 
-class FloodItWorld extends World {
+class FloodItPanel extends JPanel implements ActionListener, MouseListener, KeyListener {
   // All the cells of the game
   ArrayList<Cell> board;
   int boardSize = 10;
@@ -29,22 +31,30 @@ class FloodItWorld extends World {
   Utils u = new Utils();
   int modeNum = 1;
   ArrayList<Integer> modes = new ArrayList<Integer>(Arrays.asList(9, 10, 14, 20, 22, 24, 28));
-  TextImage result = new TextImage("", 25, Color.BLACK);
-  ArrayList<Integer> bestMovesColorCount = new ArrayList<Integer>();
+  String resultText = "";
+  Color resultColor = Color.BLACK;
   ArrayList<Cell> currentF = new ArrayList<Cell>();
   ArrayList<Cell> alreadyF = new ArrayList<Cell>();
+  Timer timer;
 
-  FloodItWorld(int boardSize, int colorNum) {
+  FloodItPanel(int boardSize, int colorNum) {
     this.boardSize = boardSize;
     this.colorNum = colorNum;
+    this.setPreferredSize(new Dimension(750, 750));
+    this.addMouseListener(this);
+    this.addKeyListener(this);
+    this.setFocusable(true);
+    this.timer = new javax.swing.Timer(10, this);
     this.initGame();
+    this.timer.start();
   }
 
   public void initGame() {
     this.clicks = 0;
     this.boardSize = this.modes.get(this.modeNum);
     this.mClicks = ((this.boardSize * this.colorNum) / 2) - ((this.boardSize * colorNum) / 5);
-    this.result = new TextImage("", 25, Color.BLACK);
+    this.resultText = "";
+    this.resultColor = Color.BLACK;
     this.alreadyF = new ArrayList<Cell>();
     this.currentF = new ArrayList<Cell>();
     this.board = new ArrayList<Cell>();
@@ -81,7 +91,6 @@ class FloodItWorld extends World {
     } else {
       this.modeNum++;
     }
-
     this.initGame();
   }
 
@@ -91,25 +100,23 @@ class FloodItWorld extends World {
     } else {
       this.colorNum++;
     }
-
     this.initGame();
   }
 
-  public WorldScene makeScene() {
-    WorldScene s = new WorldScene(0, 0);
+  protected void paintComponent(Graphics g) {
+    super.paintComponent(g);
     for (int i = 0; i < board.size(); i++) {
       Cell daCell = this.board.get(i);
-      RectangleImage cell = new RectangleImage(20, 20, OutlineMode.SOLID, daCell.color);
-      s.placeImageXY(cell, daCell.x, daCell.y);
+      g.setColor(daCell.color);
+      g.fillRect(daCell.x - 10, daCell.y - 10, 20, 20);
     }
-    s.placeImageXY(new TextImage("Size: " + this.boardSize, 25, Color.BLACK), this.boardSize * 10,
-        this.boardSize * 20 + 20);
-    s.placeImageXY(new TextImage("Colors: " + this.colorNum, 25, Color.BLACK), this.boardSize * 10,
-        this.boardSize * 20 + 50);
-    s.placeImageXY(new TextImage(this.clicks + "/" + this.mClicks, 25, Color.BLACK),
-        this.boardSize + (20 * (this.boardSize / 2)), this.boardSize * 20 + 80);
-    s.placeImageXY(result, this.boardSize * 10, this.boardSize * 20 + 110);
-    return s;
+    g.setColor(Color.BLACK);
+    g.setFont(new Font("Arial", Font.PLAIN, 25));
+    g.drawString("Size: " + this.boardSize, this.boardSize * 10, this.boardSize * 20 + 20);
+    g.drawString("Colors: " + this.colorNum, this.boardSize * 10, this.boardSize * 20 + 50);
+    g.drawString(this.clicks + "/" + this.mClicks, this.boardSize + (20 * (this.boardSize / 2)), this.boardSize * 20 + 80);
+    g.setColor(this.resultColor);
+    g.drawString(this.resultText, this.boardSize * 10, this.boardSize * 20 + 110);
   }
 
   public void flood(ArrayList<Cell> cellAr) {
@@ -139,7 +146,8 @@ class FloodItWorld extends World {
       this.flood(this.currentF);
     }
     if (this.clicks > this.mClicks) {
-      this.result = new TextImage("You lose.", 25, Color.RED);
+      this.resultText = "You lose.";
+      this.resultColor = Color.RED;
     } else {
       boolean allSame = true;
       for (int i = 0; i < board.size(); i++) {
@@ -150,58 +158,76 @@ class FloodItWorld extends World {
         }
       }
       if (allSame) {
-        this.result = new TextImage("You win!", 25, Color.GREEN);
+        this.resultText = "You win!";
+        this.resultColor = Color.GREEN;
       } else if (this.clicks == this.mClicks) {
-        this.result = new TextImage("You lose.", 25, Color.RED);
+        this.resultText = "You lose.";
+        this.resultColor = Color.RED;
       }
     }
   }
 
-  public void onMouseClicked(Posn pos, String buttonName) {
-    if (buttonName.equals("LeftButton")) {
-      Cell gotClicked = null;
-      for (int i = 0; i < board.size(); i++) {
-        Cell daCell = this.board.get(i);
-        if (pos.x >= daCell.x - 11 && pos.x < daCell.x + 9 && pos.y >= daCell.y - 11
-            && pos.y < daCell.y + 9) {
-          gotClicked = daCell;
-          break;
-        }
+  public void actionPerformed(ActionEvent e) {
+    this.onTick();
+    this.repaint();
+  }
+
+  public void mouseClicked(MouseEvent e) {
+    int posX = e.getX();
+    int posY = e.getY();
+    Cell gotClicked = null;
+    for (int i = 0; i < board.size(); i++) {
+      Cell daCell = this.board.get(i);
+      if (posX >= daCell.x - 10 && posX < daCell.x + 10 && posY >= daCell.y - 10
+          && posY < daCell.y + 10) {
+        gotClicked = daCell;
+        break;
       }
-      if (gotClicked != null) {
-        Color newColor = gotClicked.color;
-        if (newColor != this.lastColor) {
-          for (int i = 0; i < board.size(); i++) {
-            Cell daCell = this.board.get(i);
-            if (daCell.flooded) {
-              ArrayList<Cell> adjacent = new ArrayList<Cell>(
-                  Arrays.asList(daCell.left, daCell.right, daCell.top, daCell.bottom));
-              for (int j = 0; j < adjacent.size(); j++) { // write cleaner code here later
-                if (adjacent.get(j) != null && !adjacent.get(j).flooded
-                    && adjacent.get(j).color == newColor) {
-                  adjacent.get(j).flooded = true;
-                }
+    }
+    if (gotClicked != null) {
+      Color newColor = gotClicked.color;
+      if (!newColor.equals(this.lastColor)) {
+        for (int i = 0; i < board.size(); i++) {
+          Cell daCell = this.board.get(i);
+          if (daCell.flooded) {
+            ArrayList<Cell> adjacent = new ArrayList<Cell>(
+                Arrays.asList(daCell.left, daCell.right, daCell.top, daCell.bottom));
+            for (int j = 0; j < adjacent.size(); j++) {
+              if (adjacent.get(j) != null && !adjacent.get(j).flooded
+                  && adjacent.get(j).color.equals(newColor)) {
+                adjacent.get(j).flooded = true;
               }
             }
           }
-          this.lastColor = newColor;
-          this.alreadyF = new ArrayList<Cell>();
-          this.currentF = new ArrayList<Cell>(Arrays.asList(this.board.get(0)));
-          this.clicks++;
         }
+        this.lastColor = newColor;
+        this.alreadyF = new ArrayList<Cell>();
+        this.currentF = new ArrayList<Cell>(Arrays.asList(this.board.get(0)));
+        this.clicks++;
       }
     }
   }
 
-  public void onKeyEvent(String key) {
-    if (key.equalsIgnoreCase("r")) {
+  public void mousePressed(MouseEvent e) {}
+  public void mouseReleased(MouseEvent e) {}
+  public void mouseEntered(MouseEvent e) {}
+  public void mouseExited(MouseEvent e) {}
+
+  public void keyPressed(KeyEvent e) {
+    char key = e.getKeyChar();
+    if (key == 'r' || key == 'R') {
       this.initGame();
-    } else if (key.equalsIgnoreCase("s")) {
+    } else if (key == 's' || key == 'S') {
       this.changeSize();
-    } else if (key.equalsIgnoreCase("c")) {
+    } else if (key == 'c' || key == 'C') {
       this.changeColors();
     }
+    this.repaint();
   }
+
+  public void keyTyped(KeyEvent e) {}
+  public void keyReleased(KeyEvent e) {}
+
 }
 
 class Utils {
@@ -218,9 +244,13 @@ class Utils {
   }
 }
 
-class ExamplesFlood {
-  void testBigBang(Tester t) {
-    FloodItWorld w = new FloodItWorld(10, 3);
-    w.bigBang(750, 750, 0.01);
+class FloodIt {
+  public static void main(String[] args) {
+    FloodItPanel panel = new FloodItPanel(10, 3);
+    JFrame frame = new JFrame("Flood It");
+    frame.add(panel);
+    frame.pack();
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setVisible(true);
   }
 }
